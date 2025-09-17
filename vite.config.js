d6892a8622825ff5de8adc6fe7993335913d1d39
Pratyosh Desaraju <pratyosh.desaraju@gmail.com>
2025-09-16 19:41:31 -0500

    Typo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/my-website/'
+  base: './',         // Use relative paths so assets load correctly on custom domain
+  plugins: [react()]
 })
