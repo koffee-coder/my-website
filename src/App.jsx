8ab6d42df6e0cf63eea83fa917fc6726db1f8840
Pratyosh Desaraju <pratyosh.desaraju@gmail.com>
2026-01-12 19:12:56 -0600

    Switched to HashRouter to allow refreshing page in routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Particles from 'react-tsparticles';
 import { loadFull } from 'tsparticles';
 import Header from './components/Header';
